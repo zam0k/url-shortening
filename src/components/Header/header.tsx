fa0910ccfb66eae6334ae309180bc5a7c33c1a91
Kelly Castelo <kawokkun@gmail.com>
2022-06-30 11:08:19 -0300

    refactor: change way to add sass classes to tsx

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
 
 import logo from '../../assets/logo.svg';
-import './header.modules.scss';
+import styles from './header.module.scss';
 
 function Header() {
   return (
-    <header className="container">
+    <header className={styles.container}>
         <img src={logo} alt="shortly"/>
         <nav>
             <ul>
-                <li className="list"><a href="/">features</a></li>
-                <li className="list"><a href="/">pricing</a></li>
-                <li className="list"><a href="/">resources</a></li>
+                <li className={styles.list}><a href="/">features</a></li>
+                <li className={styles.list}><a href="/">pricing</a></li>
+                <li className={styles.list}><a href="/">resources</a></li>
             </ul>
             <ul>
-                <li className="list"><a href="\">login</a></li>
-                <li className="signup"><a href="\">sign up</a></li>
+                <li className={styles.list}><a href="\">login</a></li>
+                <li className={styles.signup}><a href="\">sign up</a></li>
             </ul>
         </nav>
     </header>
